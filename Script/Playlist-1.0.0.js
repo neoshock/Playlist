83b3c9270e8e7804551bd03a5a9de02c161c6f89
Josue Alcivar <pideun@gmail.com>
2019-01-20 19:32:53 -0500

    cambios realizados con exito

--- Script/Playlist-1.0.0.js
+++ Script/Playlist-1.0.0.js
@@ -9,9 +9,9 @@ function webPlayer(){
   var dataset = target.dataset;
   var type = dataset.type;
   var value = dataset.value;
-  for (var i = 0; i <= audio.length; i++) {
-    var contador = i;
+  for (var i = 0; i <= 100; i++) {
   }
+
   if (type) {
     if (type === "play") {
       var outAdio = parseFloat(value) -1;
@@ -30,13 +30,13 @@ function webPlayer(){
       btnPlay[inAudio].style.display = 'inline-block';
     }
     var stop = outAdio;
-    function stopAudio() {
+    function stopAudio(){
       trackPlay.src = audio[stop];
       trackPlay.pause();
       btnPause[stop].style.display = 'none';
       btnPlay[stop].style.display = 'inline-block';
     }
-    setTimeout(stopAudio, 20000);
+    setTimeout(stopAudio, 25000);
   }
 }
 
